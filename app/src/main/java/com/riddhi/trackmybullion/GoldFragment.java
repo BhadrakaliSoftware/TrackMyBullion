a093d15addbc8adbeb840cfacad10b783ce57482
Rahul Joshi <rahul1994joshi@gmail.com>
2017-08-05 14:37:31 -0700

    Added TabView for silver and gold prices to show seperately

---
+++ app/src/main/java/com/riddhi/trackmybullion/GoldFragment.java
@@ -0,0 +1,21 @@
+package com.riddhi.trackmybullion;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by riddhi on 8/5/2017.
+ */
+
+public class GoldFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gold_price, container, false);
+    }
+}
--- app/src/main/java/com/riddhi/trackmybullion/MainActivity.java
+++ app/src/main/java/com/riddhi/trackmybullion/MainActivity.java
@@ -1,13 +1,84 @@
 package com.riddhi.trackmybullion;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationItemView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    private void init() {
+        ButterKnife.bind(this);
+
+        bottomNavigationItemView.setItemIconTintList(null);
+        bottomNavigationItemView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        selectFragment(item);
+        return true;
+    }
+
+    private void selectFragment(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item_gold:
+                showGoldPrices();
+                break;
+            case R.id.item_silver:
+                showSilverPrices();
+                break;
+        }
+    }
+
+    private void showSilverPrices() {
+
+        //Action to show silver prices fragment to main activity
+        pushFragment(new SilverFragment());
+    }
+
+    private void showGoldPrices() {
+
+        //Action  to show gold prices fragment to main activity
+        pushFragment(new GoldFragment());
+    }
+
+    /*
+    * Method to push any fragment into given id
+    *
+    * @param fragment An instance of Fragment to show into given id
+    * */
+   protected void pushFragment(Fragment fragment) {
+       if (fragment == null)
+           return;
+
+       FragmentManager fragmentManager = getSupportFragmentManager();
+       if (fragmentManager != null) {
+           FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+           if(fragmentTransaction != null) {
+               fragmentTransaction.replace(R.id.rootLayout,fragment);
+               fragmentTransaction.commit();
+           }
+       }
+   }
 }
