458dadc5cce84cc80e927f7d7b668d80654c82bf
RAHUL JOSHI <rahul1994joshi@gmail.com>
2017-09-23 18:38:03 +0530

    removed the extra project from the project

--- app/src/main/java/com/riddhi/trackmybullion/interfaces/CurrancyChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.riddhi.trackmybullion.interfaces;
-
-/**
- * Created by riddhi on 9/3/2017.
- */
-
-public interface CurrancyChangeListener {
-    public void currencyDidChanged(String currencyName);
-}
